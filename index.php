2f2e8f3a83f719d2b44154192fffd82907953de4
Benson Mogambi <99333985+bensonmogambi@users.noreply.github.com>
2023-06-14 13:21:36 +0300

    NEW UPDATE

--- index.php
+++ index.php
@@ -1,101 +1,169 @@
 <?php
-// Read the variables sent via POST from the USSD gateway
-$sessionId = $_POST["sessionId"];
-$serviceCode = $_POST["serviceCode"];
-$phoneNumber = $_POST["phoneNumber"];
-$text = $_POST["text"];
 
-if ($text == "") {    
-    // This is the first request. Note how we start the response with "CON"
-    $response = "CON Welcome! Please provide the following information:\n";
-    $response .= "1. Customer Name\n";
-    $response .= "2. Contact\n";
-    $response .= "3. Age Range\n";
-    $response .= "4. Gender";
-} else if ($text == "1") {
-    // Business logic for capturing customer name
-    // You can store the customer name in a variable/database here
-    $response = "CON Enter Customer Name:";
-} else if ($text == "2") {
-    // Business logic for capturing contact information
-    // You can store the contact information in a variable/database here
-    $response = "CON Enter Contact Number:";
-} else if ($text == "3") {
-    // Business logic for capturing age range
-    $response = "CON Choose Age Range:\n";
-    $response .= "1. 21-34\n";
-    $response .= "2. 35-49\n";
-    $response .= "3. 50-60\n";
-    $response .= "4. Above 60";
-} else if ($text == "4") {
-    // Business logic for capturing gender
-    $response = "CON Choose Gender:\n";
-    $response .= "1. Male\n";
-    $response .= "2. Female";
-} else if ($text == "5") {
-    // Business logic for capturing seasoning cube usage
-    $response = "CON Which Seasoning Cube do you use?\n";
-    $response .= "1. MAGGI\n";
-    $response .= "2. Other";
-} else if ($text == "6") {
-    // Business logic for capturing awareness of MAGGI cube
-    $response = "CON Have you heard of or seen MAGGI cube?\n";
-    $response .= "1. Yes\n";
-    $response .= "2. No\n";
-    $response .= "3. N/A";
-} else if ($text == "7") {
-    // Business logic for capturing recent MAGGI usage
-    $response = "CON In the recent past, have you used MAGGI?\n";
-    $response .= "1. Yes\n";
-    $response .= "2. No\n";
-    $response .= "3. N/A";
-} else if ($text == "8") {
-    // Business logic for capturing dishes used with MAGGI
-    $response = "CON Which dishes do you use MAGGI on?\n";
-    $response .= "1. Meat\n";
-    $response .= "2. Chicken\n";
-    $response .= "3. Fish\n";
-    $response .= "4. Not Used\n";
-    $response .= "5. Other";
-} else if ($text == "9") {
-    // Business logic for capturing dishes used/will be used today
-    $response = "CON Which dishes have/will you use today?\n";
-    $response .= "1. Meat\n";
-    $response .= "2. Chicken\n";
-    $response .= "3. Fish\n";
-    $response .= "4. Vegetables\n";
-    $response .= "5. None\n";
-    $response .= "6. Other";
-} else if ($text == "10") {
-    // Business logic for capturing MAGGI usage frequency
-    $response = "CON How frequently do you use MAGGI?\n";
-    $response .= "1. Daily\n";
-    $response .= "2. Weekly\n";
-    $response .= "3. Monthly\n";
-    $response .= "4. Not Used";
-} else if ($text == "11") {
-    // Business logic for capturing interest in trying MAGGI
-    $response = "CON If you don't use MAGGI, would you be interested in trying it?\n";
-    $response .= "1. Yes\n";
-    $response .= "2. No\n";
-    $response .= "3. N/A";
-} else if ($text == "12") {
-    // Business logic for capturing likelihood of recommending the brand
-    $response = "CON How likely is it that you would recommend our brand to friends?\n";
-    $response .= "1. Not Likely\n";
-    $response .= "2. 1\n";
-    $response .= "3. 2\n";
-    $response .= "4. 3\n";
-    $response .= "5. 4\n";
-    $response .= "6. 5\n";
-    $response .= "7. 6\n";
-    $response .= "8. 7\n";
-    $response .= "9. 8\n";
-    $response .= "10. 9\n";
-    $response .= "11. Very Likely";
+// Function to sanitize user input
+function sanitizeInput($input) {
+    $sanitizedInput = trim($input);
+    $sanitizedInput = stripslashes($sanitizedInput);
+    $sanitizedInput = htmlspecialchars($sanitizedInput);
+    return $sanitizedInput;
 }
 
-// Send the response back to the USSD gateway
-header('Content-type: text/plain');
-echo $response;
+// Establish database connection (Replace with your own credentials)
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the user's phone number from the USSD request
+$phoneNumber = ""; // Replace with the actual method of retrieving the phone number
+
+// Initialize variables
+$accept = "";
+$salesRep = "";
+$age = "";
+$gender = "";
+$seasoningCube = "";
+$maggiUsed = "";
+$maggiDishes = "";
+$dishesUsed = "";
+$frequency = "";
+$interested = "";
+$recommendation = "";
+
+// USSD survey loop
+while (true) {
+    // Check if all the questions have been answered
+    if (!empty($accept) && !empty($salesRep) && !empty($age) && !empty($gender) && !empty($seasoningCube) && !empty($maggiUsed) && !empty($maggiDishes) && !empty($dishesUsed) && !empty($frequency) && !empty($interested) && !empty($recommendation)) {
+        break;
+    }
+
+    // USSD menu
+    if (empty($accept)) {
+        echo "CON This informs you that NESTLE KENYA will only use whatever information disclosed for internal use.\n";
+        echo "1: Accept\n";
+        echo "2: Decline\n";
+    } elseif (empty($salesRep)) {
+        echo "CON SALES REPRESENTATIVE?\n";
+    } elseif (empty($age)) {
+        echo "CON AGE\n";
+        echo "1. 21-34\n";
+        echo "2. 35-49\n";
+        echo "3. 50-60\n";
+        echo "4. Above - 60\n";
+    } elseif (empty($gender)) {
+        echo "CON GENDER\n";
+        echo "1: Male\n";
+        echo "2: Female\n";
+    } elseif (empty($seasoningCube)) {
+        echo "CON WHICH SEASONING CUBE DO YOU USE?\n";
+        echo "1: Knorr\n";
+        echo "2: Royco\n";
+        echo "3: Maggi\n";
+        echo "4: Kent\n";
+        echo "5: Jumbo\n";
+        echo "6: Others\n";
+    } elseif (empty($maggiUsed)) {
+        echo "CON IN THE RECENT PAST HAVE YOU USED MAGGI?\n";
+        echo "1: YES\n";
+        echo "2: NO\n";
+    } elseif (empty($maggiDishes)) {
+        echo "CON Which dishes do you use MAGGI on?\n";
+        echo "1: Meat\n";
+        echo "2: Fish\n";
+        echo "3: Chicken\n";
+        echo "4: Other\n";
+        echo "5: Not Used\n";
+    } elseif (empty($dishesUsed)) {
+        echo "CON WHICH DISHES HAVE/WILL YOU USE TODAY?\n";
+        echo "1: Meat\n";
+        echo "2: Chicken\n";
+        echo "3: Vegetables\n";
+        echo "4: Fish\n";
+        echo "5: None\n";
+        echo "6: Other\n";
+    } elseif (empty($frequency)) {
+        echo "CON How frequently do you use MAGGI\n";
+        echo "1: Daily\n";
+        echo "2: Weekly\n";
+        echo "3: Monthly\n";
+        echo "4: Not Used\n";
+    } elseif (empty($interested)) {
+        echo "CON If you don't use it, would you be interested to try MAGGI?\n";
+        echo "1: YES\n";
+        echo "2: NO\n";
+    } elseif (empty($recommendation)) {
+        echo "CON HOW LIKELY WOULD YOU RECOMMEND OUR BRAND TO YOUR FRIENDS?\n";
+        echo "1: Very Likely\n";
+        echo "2: Not Likely\n";
+    }
+
+    // Process user input
+    $input = sanitizeInput($_GET['input']);
+    
+    switch ($input) {
+        case "1":
+            if (empty($accept)) {
+                $accept = $input;
+            } elseif (empty($age)) {
+                $age = $input;
+            } elseif (empty($gender)) {
+                $gender = $input;
+            } elseif (empty($seasoningCube)) {
+                $seasoningCube = $input;
+            } elseif (empty($maggiUsed)) {
+                $maggiUsed = $input;
+            } elseif (empty($maggiDishes)) {
+                $maggiDishes = $input;
+            } elseif (empty($dishesUsed)) {
+                $dishesUsed = $input;
+            } elseif (empty($frequency)) {
+                $frequency = $input;
+            } elseif (empty($interested)) {
+                $interested = $input;
+            } elseif (empty($recommendation)) {
+                $recommendation = $input;
+            }
+            break;
+
+        case "2":
+            if (empty($accept)) {
+                // End the survey
+                echo "END Thank you for your response.";
+                exit();
+            } elseif (empty($maggiUsed)) {
+                $maggiUsed = $input;
+            } elseif (empty($dishesUsed)) {
+                $dishesUsed = $input;
+            } elseif (empty($frequency)) {
+                $frequency = $input;
+            } elseif (empty($interested)) {
+                $interested = $input;
+            } elseif (empty($recommendation)) {
+                $recommendation = $input;
+            }
+            break;
+
+        default:
+            // Repeat the question if the input is not valid
+            break;
+    }
+}
+
+// Store the collected data in the database
+$stmt = $conn->prepare("INSERT INTO survey_responses (phone_number, sales_rep, age, gender, seasoning_cube, maggi_used, maggi_dishes, dishes_used, frequency, interested, recommendation) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("sssssssssss", $phoneNumber, $salesRep, $age, $gender, $seasoningCube, $maggiUsed, $maggiDishes, $dishesUsed, $frequency, $interested, $recommendation);
+$stmt->execute();
+$stmt->close();
+
+// Thank you message
+echo "END Thank you for completing the survey.";
+
+// Close the database connection
+$conn->close();
+
 ?>
